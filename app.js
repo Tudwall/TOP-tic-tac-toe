fe6d0833d06c0383d11e6232111ed0a850650ea7
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-07-05 14:46:01 +0200

    add displayController module and displayController.displayBoard()
    fill gameboard.board with dummy numbers

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 // gameboard module.
 const gameboard = (() => {
-  const board = ["", "", "", "", "", "", "", "", ""];
+  const board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
 
-  return {};
+  return { board };
+})();
+
+// displayController module.
+const displayController = (() => {
+  const displayBoard = (board) => {
+    for (let i = 0; i < board.length; i++) {
+      const gridItem = document.querySelector(`[data-i="${i}"]`);
+      gridItem.textContent = board[i];
+    }
+  };
+
+  return { displayBoard };
 })();
 
 // player factory.
-const player = (name) => {
+const Player = (name) => {
   const getName = () => name;
   return { getName };
 };
